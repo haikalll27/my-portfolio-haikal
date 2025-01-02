6335ee4151f85ff524334b62a7fb624dc866c693
muhammadhaikalsch <muhammadhaikalsch@gmail.com>
2025-01-03 04:24:37 +0700

    push new

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import 'aos/dist/aos.css'
 
 // Memoized Components
 const StatusBadge = memo(() => (
-  <div className="inline-block animate-float lg:mx-0 mt-10" data-aos="zoom-in" data-aos-delay="400">
+  <div className="inline-block animate-float lg:mx-0 mt-10 sm:mt-2 md:mt-2 lg:mt-1 pt-10 sm:pt-2 md:pt-2 lg:pt-1" data-aos="zoom-in" data-aos-delay="400">
     <div className="relative group">
       <div className="absolute -inset-0.5 bg-gradient-to-r from-[#6366f1] to-[#a855f7] rounded-full blur opacity-30 group-hover:opacity-50 transition duration-1000"></div>
       <div className="relative px-3 sm:px-4 py-2 rounded-full bg-black/40 backdrop-blur-xl border border-white/10">
