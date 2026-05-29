8d22ff6fab8f6016589d6786e55d41d8bf5ece61
haikalll27 <muhammadhaikalsch@gmail.com>
2026-05-29 17:50:51 +0700

    (update) fixing

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -9,7 +9,7 @@ const Header = memo(() => (
   <div className="text-center lg:mb-8 mb-2 px-[5%]">
     <div className="inline-block relative group">
       <h2 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#6366f1] to-[#a855f7]" data-aos="zoom-in-up" data-aos-duration="600">
-        About Me
+        About Me haikal
       </h2>
     </div>
     <p className="mt-2 text-gray-400 max-w-2xl mx-auto text-base sm:text-lg flex items-center justify-center gap-2" data-aos="zoom-in-up" data-aos-duration="800">
